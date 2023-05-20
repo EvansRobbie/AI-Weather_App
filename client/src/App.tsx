ef300608735f0ee14b484f64d3ac7d5f9b197959
EvansRobbie <evansitoden94@gmail.com>
2023-05-20 14:42:27 +0300

    spinner add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
+import axios from 'axios'
 import './App.css'
 import Navbar from './components/Navbar'
 import Weather from './components/Weather'
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+// console.log('VITE_API_BASE_URL:', axios.defaults.baseURL);
+axios.defaults.withCredentials =  true
 
 function App() {
   return (
---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import spinner from '../assets/spinner.svg'
+
+const Spinner = () => {
+  return (
+    <div className='flex items-center'>
+        <img src={spinner} alt="" />
+    </div>
+  )
+}
+
+export default Spinner
