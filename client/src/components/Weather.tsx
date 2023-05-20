97d16666d597ba69ff9263fbbb7e3040efe503f5
EvansRobbie <evansitoden94@gmail.com>
2023-05-20 14:51:18 +0300

    typescript fix

---
+++ client/src/components/Weather.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import WeatherItem from "./WeatherItem";
+
+import axios from "axios";
+interface weatherProps {
+  weather: {
+    id: number;
+    main: string;
+    icon: string;
+    description: string;
+  }[];
+  sys: {
+    country: string;
+  };
+  name: string;
+  main: {
+    humidity: number;
+    temp: number;
+  };
+  wind: {
+    speed: number;
+  };
+}
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const Weather = () => {
+  const [city, setCity] = useState<string>("");
+  const [weather, setWeather] = useState<weatherProps | null>(null);
+  const [loading, setIsLoading] = useState<boolean>(false);
+  const [prediction, setPrediction] = useState<string[] | []>([]);
+  // const [clothingSuggestions, setClothingSuggestions] = useState([]);
+  // console.log(apiKey)
+
+  const fetchPrediction = async () => {
+    const response = await axios.post("/weather", { city });
+    //  const { suggestions } = ;
+    setPrediction(response.data);
+  };
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setCity(input);
+    // autoCompleteCityName(input)
+  };
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchPrediction();
+    setIsLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data);
+        setIsLoading(false);
+      })
+      .catch((e) => console.log(e));
+    // setIsLoading(false)
+    setCity("");
+  };
+  // console.log(weather)
+  // console.log(loading)
+
+  return (
+    <div className="relative w-full h-screen lg:h-[120vh] max-w-full px-4 top-[50%] py-6 left-0 mx-auto flex items-center justify-center">
+      <img
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+        src="https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?cs=srgb&dl=pexels-pixabay-209831.jpg&fm=jpg"
+        alt=""
+      />
+      <div className=" px-4 w-full md:max-w-[50vw] mt-24 lg:max-w-[40vw] backdrop-blur backdrop-filter border translation hover:border-l-8 border-l-2 border-l-cyan-500 h-auto bg-slate-900/10 rounded-2xl">
+        <form onSubmit={onSubmit}>
+          <div className="flex gap-4 items-center justify-center w-full py-10 h-full">
+            <div className="flex flex-col justify-center w-full">
+              <label
+                htmlFor="city"
+                className="uppercase text-slate-200 text-center font-bold text-sm"
+              >
+                City name
+              </label>
+              <input
+                type="text"
+                placeholder="current weather in your City"
+                value={city}
+                onChange={handleCityChange}
+                className="px-4 py-1.5 w-full outline-none focus:border-cyan-500 translation border-b-2 text-slate-200 placeholder:text-slate-600 bg-transparent rounded-xl"
+                required
+              />
+            </div>
+            <button className="inline-block px-4 py-1.5 lg:py-2 w-20 lg:w-32 hover:bg-slate-200 hover:text-slate-950 hover:shadow-slate-950 shadow-2xl  hover:border-l-4 hover:border-r-4 border-cyan-500 font-bold bg-slate-950 text-slate-100 rounded-lg text-sm translation">
+              Search
+            </button>
+          </div>
+        </form>
+        {weather && (
+          <WeatherItem
+            weather={weather}
+            loading={loading}
+            prediction={prediction}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
--- client/src/components/WeatherItem.tsx
+++ client/src/components/WeatherItem.tsx
@@ -1,6 +1,82 @@
 import React from 'react'
 
-const WeatherItem = () => {
+ interface weatherProp {
+    loading: boolean
+    prediction: string[]
+    weather:{
+        weather:{
+            id:number
+            main:string
+            description:string
+            icon:string
+        }[]
+        main:{
+            temp:number
+            // wind:number
+            humidity:number
+        }
+        wind:{
+            speed:number
+        }
+        sys:{
+            country:string
+        }
+        name:string
+    }
+
+}
+const WeatherItem: React.FC<weatherProp> = ({weather, loading, prediction}) => {
+
+    const weatherElement = weather.weather.map((currentWeather)=>{
+        const {id, main, description, icon} = currentWeather
+        return(
+            <div key={id} className="w-full h-full bg-slate-100/50 rounded-xl px-4 py-3 " >
+                <div className="w-full flex justify-between mb-2 gap-4 ">
+
+                    <div className="relative">
+                        <h2 className=" text-slate-900 font-semibold text-xl">{weather.name}</h2>
+                        <h3 className="absolute -right-4 top-4 z-10 opacity-100 text-xs text-cyan-900">{weather.sys.country}</h3>
+                    </div>
+                    <div className="ml-7">
+                        <h4 className="text-xl font-bold text-slate-900">{main}</h4>
+                        <p className="text-slate-900 text-xs font-medium">{description}&nbsp;</p>
+                    </div>
+                    <div className="h-16 bg-slate-900/30 rounded-lg">
+
+                    <img className=" drop-shadow animate-bounce " src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={`/${weather.name}`} />
+                    </div>
+                </div>
+                <div className="flex w-full justify-around   pb-4 ">
+                    <div>
+                        {/* humidity */}
+                        <div className="flex items-center">
+                        <p className="text-3xl text-slate-900">{weather.main.humidity}</p>
+                        <span>%</span>
+                        </div>
+                   
+                        <h5>Humidity</h5>
+                    </div>
+                    <div>
+                        {/* feels_like */}
+                        <div className="flex items-center gap-1">
+                            <p className="text-3xl text-slate-900">{weather.wind.speed.toFixed(0)}</p>
+                            <span className="text-sm">MPH</span>
+
+                        </div>
+                        <h5>Feels Like</h5>
+                    </div>
+                    <div>
+                        {/* temp */}
+                        <p className="text-4xl text-slate-900 font-semibold">{(weather.main.temp - 273.25).toFixed(0)}&#176;</p>
+                        <h5>Temperature</h5>
+                    </div>
+                </div>
+                
+                
+            </div>
+        )
+           
+    })
   return (
     <div>WeatherItem</div>
   )
